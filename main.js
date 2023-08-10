e6f2a41839fa52ce79c411b8c2796e2dd4368fc2
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-10 16:17:14 -0400

    eightBall varialbe created with a value of an empty string

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let userName = 'Kam';
 let userQuestion = 'How old am I?';
 let randomNumber =  Math.floor(Math.random() * 8);
+let eightBall = '';
 
 userName !== '' ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
 
