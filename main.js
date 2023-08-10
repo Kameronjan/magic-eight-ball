ba52401ff6e72735d1de620c2a670ae403a1fcf1
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-10 16:31:10 -0400

    saving before refactoring code

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ let eightBall = randomNumber;
 if (eightBall === 1) {
   console.log('It is certain');
 } else if (eightBall === 2) {
-  console.log('It is decidedly so')
+  console.log('It is decidedly so');
 } else if (eightBall === 3) {
-  console.log('Reply hazy try again')
+  console.log('Reply hazy try again');
 } else if (eightBall === 4) {
-  console.log('Cannot predict now')
+  console.log('Cannot predict now');
 } else if (eightBall === 5) {
-  console.log('Do not count on it')
+  console.log('Do not count on it');
 } else if (eightBall === 6) {
-  console.log('Outlook not so good')
+  console.log('Outlook not so good');
 } else if (eightBall === 7) {
-  console.log('Signs point to yes')
+  console.log('Signs point to yes');
 }
 
 userName !== "" ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 
-console.log(userQuestion);
+console.log(eightBall);
