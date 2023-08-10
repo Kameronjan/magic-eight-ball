21cc237f75fa84702fc7310bb15a707b2610df4b
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-10 16:43:17 -0400

    switched my if/else statement to a switch case

--- main.js
+++ main.js
@@ -8,22 +8,34 @@ let randomNumber = Math.floor(Math.random() * 8);
 
 console.log(randomNumber);
 
-let eightBall = randomNumber;
+let eightBall = '';
 
-if (eightBall === 1) {
-  console.log('It is certain');
-} else if (eightBall === 2) {
-  console.log('It is decidedly so')
-} else if (eightBall === 3) {
-  console.log('Reply hazy try again')
-} else if (eightBall === 4) {
-  console.log('Cannot predict now')
-} else if (eightBall === 5) {
-  console.log('Do not count on it')
-} else if (eightBall === 6) {
-  console.log('Outlook not so good')
-} else if (eightBall === 7) {
-  console.log('Signs point to yes')
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
 
 userName !== "" ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 
