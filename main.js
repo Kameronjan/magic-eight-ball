de84ed52cb3a27b61f40b48cc78501eb65178ec8
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-10 16:20:08 -0400

    re assigned eightBall to randomNumber

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let userName = 'Kam';
-let userQuestion = 'How old am I?';
-let randomNumber =  Math.floor(Math.random() * 8);
-let eightBall = '';
+let userName = "Kam";
+let userQuestion = "How old am I?";
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = randomNumber;
 
-userName !== '' ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+userName !== "" ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 
 console.log(userQuestion);
