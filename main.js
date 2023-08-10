bd665d6f5bb105efc9cbf92046f8083150947fb1
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-10 16:16:15 -0400

    few changes made, created user question, random number variable and working on eight ball next.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-let userName = '';
+let userName = 'Kam';
+let userQuestion = 'How old am I?';
+let randomNumber =  Math.floor(Math.random() * 8);
 
 userName !== '' ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+console.log(userQuestion);
