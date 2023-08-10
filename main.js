5582bf6a89956f5e3f4ed8c2e3ce3162d6ae3cf0
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-08-10 16:38:39 -0400

    first part of my js refactored

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-let userName = "Kam";
-let userQuestion = "How old am I?";
+userName ? console.log(`Hello, ${userName}`) : console.log(`Hello!`);
+
+let userQuestion =  `Will I have a baby anytime soon?`;
+
+console.log(`${userName} has asked - ${userQuestion}`);
+
 let randomNumber = Math.floor(Math.random() * 8);
+
+console.log(randomNumber);
+
 let eightBall = randomNumber;
 
 if (eightBall === 1) {
   console.log('It is certain');
 } else if (eightBall === 2) {
-  console.log('It is decidedly so');
+  console.log('It is decidedly so')
 } else if (eightBall === 3) {
-  console.log('Reply hazy try again');
+  console.log('Reply hazy try again')
 } else if (eightBall === 4) {
-  console.log('Cannot predict now');
+  console.log('Cannot predict now')
 } else if (eightBall === 5) {
-  console.log('Do not count on it');
+  console.log('Do not count on it')
 } else if (eightBall === 6) {
-  console.log('Outlook not so good');
+  console.log('Outlook not so good')
 } else if (eightBall === 7) {
-  console.log('Signs point to yes');
-}
+  console.log('Signs point to yes')
 
 userName !== "" ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 
